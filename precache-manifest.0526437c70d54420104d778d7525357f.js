41f3d80a7a1e7831977c50247d21b9f4b2856818
fachrulhakim479@gmail.com <alifsyahputra1612@gmail.com>
2022-05-18 15:59:18 +0700

    Updates

---
+++ precache-manifest.0526437c70d54420104d778d7525357f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db835cde3a0d39d1649f",
+    "url": "/alquranku/static/css/main.759b50e6.chunk.css"
+  },
+  {
+    "revision": "db835cde3a0d39d1649f",
+    "url": "/alquranku/static/js/main.db835cde.chunk.js"
+  },
+  {
+    "revision": "9bf32f55384023627f7f",
+    "url": "/alquranku/static/js/1.9bf32f55.chunk.js"
+  },
+  {
+    "revision": "4cae9fdc97010a412136",
+    "url": "/alquranku/static/js/2.4cae9fdc.chunk.js"
+  },
+  {
+    "revision": "33887f49a40e0f5e2ba9",
+    "url": "/alquranku/static/js/runtime~main.33887f49.js"
+  },
+  {
+    "revision": "82ff622df253c3668d90b02a325a0c3d",
+    "url": "/alquranku/index.html"
+  }
+];
